0dc97a4ad3f97147edb413a84905931e08628d7c
ruriz <lingrz2@uw.edu>
2021-05-23 15:10:39 -0700

    helper modified

--- helper.py
+++ helper.py
@@ -5,12 +5,12 @@ import re
 def preprocess(filepath):
     with open(filepath, 'r') as f:
         sentences = f.readlines()
+        output = []
         for sentence in sentences:
             sentence = sentence.strip("\n")
             sentence = sentence.translate(str.maketrans('', '', string.punctuation))
+            # set 1+whitespace to only one whitespace for split
             sentence = re.sub(pattern=r'[\s]+', repl = ' ', string=sentence)
-        
-
-def sen_to_tokens(sentences):
-    for sentence in sentences:
-        pass
+            # sentence here becomes a list of tokens
+            output.append(sentence.split())
+    return output
