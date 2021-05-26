784128e0313f77ac3a371e463cec3c83c89402c5
vnhavel <vnhavel@gmail.com>
2021-05-26 16:55:33 -0700

    Unigram Partial

--- helper.py
+++ helper.py
@@ -1,7 +1,7 @@
 import string
 import re
 
-def preprocess(filepath):
+def preprocess(filepath, n):
     with open(filepath, 'r') as f:
 
         sentences = f.readlines() # creates a list of lines from the from [line 1, line 2, ...]
@@ -10,7 +10,28 @@ def preprocess(filepath):
             sentence = sentence.strip("\n") # Gets rid \n characters [line 1, The children are all fond of him .", ...]
             sentence = sentence.translate(str.maketrans('', '', string.punctuation)) # remove punctuation [line 1, The children are all fond of him , ...]
             sentence = re.sub(pattern=r'[\s]+', repl = ' ', string=sentence) # remove extra white space [line 1, The children are all fond of him , ...]
+            if n == 2:
+                sentence = ("<s> " + sentence + " </s>")
+            if n == 3:
+                sentence = ("<s_1> <s_2> " + sentence + " </s>")
             output.append(sentence.split()) # splits sentence into a list of words [line 1, [The, children, are, all, fond, of, him] , ...]
-    return output
+    print (output)
 
-preprocess("C:\\Users\\nguye\\Documents\\LING 472\\Term Project\\finalproject-ling472\\data")
+def count_tokens(sentences):
+    # create a token dictionary
+    token_dict = {}
+    # count how many tokens are there in the entire text
+    N = 0
+    # for each sentence in the sentence list
+    for sentence in sentences:
+        # for each token in the sentence we are looking at
+        for token in sentence:
+            N += 1
+            # if this is a new word
+            if token not in token_dict.keys():
+                # put this word into the key
+                token_dict[token] = 0
+            # add one count for the word
+            token_dict[token] += 1
+    return (N, token_dict)
+    # token_dict = count_tokens(sentences)[1]
--- unigram.py
+++ unigram.py
@@ -5,10 +5,19 @@
 class LanguageModel:
 
     def __init__(self):
-        pass
+        self.processed_text = []
+        self.vocab = {} # {vocab;occurances}
+        self.unigram = {} # {unigram = vocab;probability = occurances + 1/total + |V|}
+
 
     def train(self, train_corpus):
-        print('I am an unimplemented UNIGRAM train() method.')  # delete this!
+        self.processed_text = preprocess(train_corpus, 1) # process raw text file into list of (sentences) lists of words
+        self.vocab = count_tokens(self.processed_text)[1] # Add each word(token) to vocab dictionary and its # of occurences
+        total = count_tokens(self.processed_text)[0]
+        for token in self.vocab:
+            self.unigram[token] = (self.vocab[token] + 1) / (total + len(self.vocab)) # Store token into unigram dictionary with its probability
+        print(self.unigram)
+
 
     def score(self, test_corpus):
         print('I am an unimplemented UNIGRAM score() method.')  # delete this!
